fc73c549f33a71d945b47678e5dddc9399fb8fe6
JoshuaPeters <jgp2.11.99@gmail.com>
2024-04-29 22:17:59 -0600

    Implemented the valide method for teams in team.controller.js

--- app/controllers/team.controller.js
+++ app/controllers/team.controller.js
@@ -1,3 +1,25 @@
 const Team = require("../models/team.model.js");
 const { body, validationResult } = require('express-validator');
 
+exports.validate = (method) => {
+    let rules = [
+        body('name', 'name cannot be empty').not().isEmpty().trim().escape(),
+        body('notes').trim().escape(),
+        body('coach_id', 'coach_id cannot be empty').not().isEmpty().trim().escape(),
+        body('league_id', 'league_id cannot be empty').not().isEmpty().trim().escape(),
+        body('motto').trim().escape()
+    ]
+    switch (method) {
+        case 'updateTeam':
+            return rules;
+        case 'createTeam': {
+            let createRules = [...rules];
+            createRules.push(
+                body('name').custom(async (value) => {
+                    return await Team.checkDuplicateName(value);
+                })
+            )
+            return createRules;
+        }
+    }
+}
