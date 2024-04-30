f785d65a61d9a0b6936301e640964620a3524f01
JoshuaPeters <jgp2.11.99@gmail.com>
2024-04-29 19:49:19 -0600

    Implemented the list method in team.model.js

--- app/models/team.model.js
+++ app/models/team.model.js
@@ -19,3 +19,26 @@ Team.create = async (newTeam, result) => {
     }
 };
 
+Team.list = async (params, result) => {
+    let query = `SELECT id, name, coach_id, league_id, notes, motto
+    FROM (SELECT temp.id, temp.name, temp.coach_id, temp.league_id, temp.notes, temp.motto
+    FROM teams temp) as x`;
+
+    if (params.filterCol && params.filterStr) {
+        query += ` WHERE x.${params.filterCol} LIKE '%${params.filterStr}%'`;
+    }
+
+    if (params.sortCol && params.sortDir) {
+        query += ` ORDER BY x.${params.sortCol} ${params.sortDir}`;
+    }
+
+    console.log(query);
+
+    try {
+        let result = await sql.query(query);
+        return result;
+    } catch (err) {
+        console.log("error: ", err);
+        throw(err);
+    }
+};
